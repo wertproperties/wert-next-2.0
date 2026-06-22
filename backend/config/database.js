9ceb880809eaee5e970a272808883c9f8b940d2c
wertproperties <wertproperties@gmail.com>
2026-06-23 00:08:25 +0530

    signup page fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hausverwaltung', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://wertproperties_db_user:hA3uzabzpM7xD4nK@wert.zfawce4.mongodb.net/?appName=WERT', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
